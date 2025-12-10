3fa53045f0e881514d63bc902225eef4273b968d
nikola240698 <nikola240698@gmail.com>
2025-12-11 00:07:01 +0500

    Create swapNumbers.cpp

---
+++ lesson_3.5/swapNumbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    
+    int a, b;
+
+    std::cout << "Программа для замены местами двух переменных!" << std::endl;
+    std::cout << "Введите первую переменную: ";
+    std::cin >> a;
+    std::cout << "Введите вторую переменную: ";
+    std::cin >> b;
+
+    std::cout << "Вы ввели а=" << a << " и b=" << b << std::endl;
+
+    a = a + b;
+    b = a-b;
+    a = a-b;
+
+    std::cout << "Получаем a=" << a << " и b=" << b << std::endl;
+
+}
