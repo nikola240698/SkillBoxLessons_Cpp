749d73fa9322b26affb8dbedc32103e3cdd2ee22
nikola240698 <nikola240698@gmail.com>
2025-12-11 14:49:42 +0500

    Create barberNumber.cpp

---
+++ lesson_4.2/barberNumber.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+int main() {
+    
+    int dayInMonth      = 30;       //кол-во дней в месяце
+    int barberShift     = 8;        //часов в смене барбера
+    int barberSpeed     = 1;        //колчисетву мужчин за 1 час работы барбера
+
+    int manNumber, barberNumberInStock, barberNumberNeed;
+    int barberPowerMonth;
+    int totalMonthNumber;
+
+    std::cout << "Пограмма для вычисления необходимого колличества барберов" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Укаэите, сколько мужчин проживает в городе? ";
+    std::cin >> manNumber;
+    std::cout << "Укажте, сколько барберов уже работают в городе? ";
+    std::cin >> barberNumberInStock;
+    std::cout << std::endl;
+    std::cout << "----------- Производим расчеты -----------" << std::endl;
+    std::cout << std::endl;
+
+    barberPowerMonth = barberSpeed * barberShift * dayInMonth;
+
+    std::cout << "При скорости работы барбера в " << barberSpeed << " муж/ч" << std::endl;
+    std::cout << "И смене в " << barberShift << " ч, его производительность ";
+    std::cout << barberSpeed * barberShift << " муж/смен" << std::endl;
+    std::cout << "За месяц длиной в " << dayInMonth << " дней его производительность ";
+    std::cout << barberPowerMonth << " муж/мес" << std::endl;
+
+    totalMonthNumber = manNumber / barberPowerMonth;
+
+    std::cout << "Для успешной компании требуется " << totalMonthNumber << " барберов" << std::endl;
+    std::cout << "------------------ Получаем итог ------------------" << std::endl;
+
+    if (totalMonthNumber > barberNumberInStock) {
+        std::cout << "Для успешной работы нам необходимо еще ";
+        std::cout << totalMonthNumber - barberNumberInStock << " барберов" << std::endl;
+    }
+
+    if (totalMonthNumber == barberNumberInStock) {
+        std::cout << "Наши барберы справляются успешно!" << std::endl;
+    }
+
+    if (totalMonthNumber < barberNumberInStock) {
+        std::cout << "Наши барберы работают не всю смену" << std::endl;
+        std::cout << "Предлагаем сократить " << barberNumberInStock - totalMonthNumber << " барберов" << std::endl;
+    }
+
+
+
+
+    //barberNumberNeed =
+
+
+
+
+}
