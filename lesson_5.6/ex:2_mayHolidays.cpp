a38607a9c01548c37ac98f84b4557c5466a76440
nikola240698 <nikola240698@gmail.com>
2025-12-14 14:38:28 +0500

    Create ex:2_mayHolidays.cpp

---
+++ lesson_5.6/ex:2_mayHolidays.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+
+	int day;
+
+	std::cout << "Для оперделения рабочий или выходной введеный день мая\n\n";
+	std::cout << "Введите интересующий день:";
+	std::cin >> day;
+	std::cout << "------------ Проверяем ------------" << std::endl;
+  
+	if (day > 0 && day <=31) {
+		if (day <=5 || (day >=8 && day <= 10) || (day + 1) % 7 == 0 || day % 7 == 0) {
+			std::cout << "Выходной день!" << std::endl;
+		} else {
+			std::cout << "Рабочий день!" << std::endl;
+		}
+	} else std::cout << "Не верно введен номер дня!" << std::endl;
+  
+}
