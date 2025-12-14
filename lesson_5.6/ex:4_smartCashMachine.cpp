0c4f73d9628360b6dda358d9c2bfcdb04cec0aff
nikola240698 <nikola240698@gmail.com>
2025-12-14 20:04:26 +0500

    Update ex:4_smartCashMachine.cpp

--- lesson_5.6/ex:4_smartCashMachine.cpp
+++ lesson_5.6/ex:4_smartCashMachine.cpp
@@ -3,7 +3,7 @@
 int main() {
 
 	int cashPull;
-  
+
 	std::cout << "Программа для проверки возможности выдачи \n";
 	std::cout << "наличных банкоматом оптимальными купюрами \n";
 
@@ -17,39 +17,38 @@ int main() {
 		std::cout << "Банкомат может выдать вам ";
 		if (cashPull >= 5000) {
 		  int cashOnHand = cashPull / 5000;
-			std::cout << cashOnHand << " по 5000, ";
-			cashPull = cashPull - (cashOnHand * 5000);
+			std::cout << cashOnHand << " по 5000р, ";
+			cashPull -= (cashOnHand * 5000);
 		}
 
 		if (cashPull >= 2000) {
 				int cashOnHand = cashPull / 2000;
-				std::cout << cashOnHand << " по 2000, ";
-				cashPull = cashPull - (cashOnHand * 2000);
+				std::cout << cashOnHand << " по 2000р, ";
+				cashPull -= (cashOnHand * 2000);
 				}
 
 		if (cashPull >= 1000) {
 				int cashOnHand = cashPull / 1000;
-				std::cout << cashOnHand << " по 1000, ";
-				cashPull = cashPull - (cashOnHand * 1000);
+				std::cout << cashOnHand << " по 1000р, ";
+				cashPull -= (cashOnHand * 1000);
 				}
 
 		if (cashPull >= 500) {
 				int cashOnHand = cashPull / 500;
-				std::cout << cashOnHand << " по 500, ";
-				cashPull = cashPull - (cashOnHand * 500);
+				std::cout << cashOnHand << " по 500р, ";
+				cashPull -= (cashOnHand * 500);
 				}
 
 		if (cashPull >= 200) {
 				int cashOnHand = cashPull / 200;
-				std::cout << cashOnHand << " по 200, ";
-				cashPull = cashPull - (cashOnHand * 200);
+				std::cout << cashOnHand << " по 200р, ";
+				cashPull -= (cashOnHand * 200);
 				}
 
 		if (cashPull >= 100) {
 				int cashOnHand = cashPull / 100;
-				std::cout << cashOnHand << " по 100, ";
-				cashPull = cashPull - (cashOnHand * 100);
+				std::cout << cashOnHand << " по 100р, ";
+				cashPull -= (cashOnHand * 100);
 				}
 	}
-
 }
