46f6a366e29e3e8bd38e09992b38abe2f0bf861c
nikola240698 <nikola240698@gmail.com>
2025-12-21 20:05:11 +0500

    Create ex:8_christmasTree.cpp

---
+++ lesson_7.5/ex:8_christmasTree.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int treeHeight;
+	int sharpAmount = 1;
+
+	cout << "Программа для отображения ёлочки" << endl << endl;
+	cout << "Введите высоту ёлочки: ";
+	cin >> treeHeight;
+
+	// цикл отображения
+	for (int row = 1; row <= treeHeight; row++) {
+
+		// отобрадаем пустые пробелы
+		for (int space = 0; space < (treeHeight - row); space++) cout << " ";
+
+		// тображаем решетки
+		for (int sharp = 0; sharp < sharpAmount; sharp++) cout << "#";
+
+		// на каждой строке увеличиваем на 2 кол-во решеток
+		sharpAmount += 2;
+
+		cout << endl;
+
+	}
+
+}
+
