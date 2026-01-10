025e6092fbcbb3f190708e6da4772c23a55a3e2f
nikola240698 <nikola240698@gmail.com>
2026-01-10 15:14:14 +0500

    Create ex:3_ringBuffer.cpp

---
+++ lesson_13.6/ex:3_ringBuffer.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+
+#define BUFFER_SIZE (20)
+
+void addToBuffer(std::vector<int> &vec, const int &value, int pos) {
+    if (pos > BUFFER_SIZE - 1) {
+        pos = (pos % BUFFER_SIZE);
+    }
+    vec[pos] = value;
+}
+
+void showBuffer(const std::vector<int> &vec) {
+    std::cout << "{ ";
+    for (int i = 0; i < vec.capacity(); i++) {
+        std::cout << vec[i] << " ";
+    }
+    std::cout << "}" << std::endl;
+}
+
+int inputNum() {
+    while(true)
+    {
+        int value;
+        std::cout << "Input number: ";
+        std::cin >> value;
+        if (std::cin.fail() || std::cin.peek() != '\n')
+        {
+            std::cerr << "Incorrect data" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        else
+            return value;
+    }
+}
+
+int main() {
+
+    int n;
+    int count = 0;
+    std::vector<int> buffer(BUFFER_SIZE);
+    buffer.clear();
+
+    while (true) {
+        n = inputNum();
+        if (n == -1)
+            showBuffer(buffer);
+        else {
+            addToBuffer(buffer, n, count);
+            count++;
+        }
+    }
+    return 0;
+}
